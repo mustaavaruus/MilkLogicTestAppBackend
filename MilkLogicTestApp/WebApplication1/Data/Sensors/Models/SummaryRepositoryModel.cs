c1c6785f9127309c9b5b135658abdd8de0901d39
mustaavaruus <64552305+mustaavaruus@users.noreply.github.com>
2026-03-16 22:04:08 +0300

    initial

---
+++ MilkLogicTestApp/WebApplication1/Data/Sensors/Models/SummaryRepositoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Data.Sensors.Models
+{
+    public class SummaryRepositoryModel
+    {
+        public float? Average { get; set; }
+        public float? Maximum { get; set; }
+        public float? Minimum { get; set; }
+    }
+}
---
+++ MilkLogicTestApp/WebApplication1/Services/Sensors/Models/SummaryServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Services.Sensors.Models
+{
+    public class SummaryServiceModel
+    {
+        public float? Average { get; set; }
+        public float? Maximum { get; set; }
+        public float? Minimum { get; set; }
+    }
+}
