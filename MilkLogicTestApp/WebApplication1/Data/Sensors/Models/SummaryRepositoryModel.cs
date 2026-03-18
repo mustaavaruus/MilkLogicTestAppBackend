d80b7346d3d83bd727dc0bf27d2eb5c2645d600c
mustaavaruus <64552305+mustaavaruus@users.noreply.github.com>
2026-03-18 23:30:48 +0300

    remove folder + some props

--- MilkLogicTestApp/WebApplication1/Data/Sensors/Models/SummaryRepositoryModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Data.Sensors.Models
-{
-    public class SummaryRepositoryModel
-    {
-        public float? Average { get; set; }
-        public float? Maximum { get; set; }
-        public float? Minimum { get; set; }
-    }
-}
--- MilkLogicTestApp/WebApplication1/Services/Sensors/Models/SummaryServiceModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Services.Sensors.Models
-{
-    public class SummaryServiceModel
-    {
-        public float? Average { get; set; }
-        public float? Maximum { get; set; }
-        public float? Minimum { get; set; }
-    }
-}
